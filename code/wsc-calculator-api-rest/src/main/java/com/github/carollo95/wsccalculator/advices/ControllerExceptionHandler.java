f250d0f61bd64928d658e5565af7839c739ede81
Pedro <carollo95@gmail.com>
2023-07-04 20:55:06 +0200

    #13 Added one more exception to the handler and more logging.
    The tracing advice is not enough for logging

--- code/wsc-calculator-api-rest/src/main/java/com/github/carollo95/wsccalculator/advices/ControllerExceptionHandler.java
+++ code/wsc-calculator-api-rest/src/main/java/com/github/carollo95/wsccalculator/advices/ControllerExceptionHandler.java
@@ -3,17 +3,15 @@
  *  See file LICENSE file for full license details.
  */
 
-/*
- *  This file is part of wsc-Calculator.
- *  See file LICENSE file for full license details.
- */
-
 package com.github.carollo95.wsccalculator.advices;
 
+import lombok.extern.slf4j.Slf4j;
+import org.apache.commons.lang3.exception.ExceptionUtils;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.HttpStatusCode;
 import org.springframework.http.ResponseEntity;
 import org.springframework.http.converter.HttpMessageNotReadableException;
+import org.springframework.web.HttpMediaTypeNotAcceptableException;
 import org.springframework.web.HttpMediaTypeNotSupportedException;
 import org.springframework.web.HttpRequestMethodNotSupportedException;
 import org.springframework.web.bind.MethodArgumentNotValidException;
@@ -23,6 +21,7 @@ import org.springframework.web.bind.annotation.ExceptionHandler;
 /**
  * Advice that captures all the exceptions thrown from the Controller layer and properly returns the appropriate response.
  */
+@Slf4j
 @ControllerAdvice
 public class ControllerExceptionHandler {
 
@@ -39,7 +38,8 @@ public class ControllerExceptionHandler {
     @ExceptionHandler({
             HttpMessageNotReadableException.class,
             MethodArgumentNotValidException.class,
-            HttpMediaTypeNotSupportedException.class
+            HttpMediaTypeNotSupportedException.class,
+            HttpMediaTypeNotAcceptableException.class
     })
     private ResponseEntity<String> badParametersHandler(final Exception e) {
         return ResponseEntity.badRequest().body("The parameters are not valid");
@@ -63,6 +63,7 @@ public class ControllerExceptionHandler {
      */
     @ExceptionHandler
     private ResponseEntity<String> genericExceptionHandler(final Exception e) {
+        log.error(ExceptionUtils.getStackTrace(e));
         return new ResponseEntity<>("An internal error occurred. Please contact the administrator",
                 HttpStatus.INTERNAL_SERVER_ERROR);
     }
